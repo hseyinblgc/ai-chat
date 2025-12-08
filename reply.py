fac5058b2d98eff4da9a2b6ce7e8833c0b8cae53
hseyinblgc <h.bilgic1818@gmail.com>
2025-12-08 15:02:36 +0300

    code cleanup

--- reply.py
+++ reply.py
@@ -1,44 +1,32 @@
-from google import genai
-import torch
-import sounddevice as sd
-import threading
+import os
 
-apikey = genai.Client(api_key="YOUR_API_KEY_HERE")
+from dotenv import load_dotenv
+from google import genai
+from PyQt6 import QtCore, QtGui, QtWidgets
+from mainwindow import Ui_MainWindow
+import sys
+
+load_dotenv()
+
+apikey = genai.Client(api_key=os.getenv("API_KEY"))
 
 def callapi(user_input):
     response = apikey.models.generate_content(
         model="gemini-2.5-flash", contents=user_input
     )
-    out = response.text
-    if len(out) < 1000: 
-        # Ses çıkışını ayrı thread'de başlat
-        audio_thread = threading.Thread(target=rise, args=(response.text,))
-        audio_thread.daemon = True  # Ana program kapandığında thread'i sonlandır
-        audio_thread.start()
-    
-    return response  # yield yerine return
+    return response.text
 
-## Voice model
-is_running = True
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
 
-language = 'en'
-model_id = 'v3_en'
-device = torch.device('cpu')
-
-model, example_text = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                      model='silero_tts',
-                                      language=language,
-                                      speaker=model_id)
-model.to(device)
-
-sample_rate = 48000
-speaker = 'en_0'
-
-# let me hear
-def rise(speak):
-    audio = model.apply_tts(text=speak,
-                            speaker=speaker,
-                            sample_rate=sample_rate)
-    
-    sd.play(audio.numpy(), sample_rate)
-    sd.wait()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+# ----------------------
+# RUN
+# ----------------------
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
